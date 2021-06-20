c36b3c567d7b6a20bebbc5737b3628e22a6b2776
LottieDeane <71388149+LottieDeane@users.noreply.github.com>
2021-06-20 14:29:07 +0100

    Create kitchen.ino

---
+++ kitchen.ino
@@ -0,0 +1,35 @@
+
+#include <RCSwitch.h>
+int led = 12;
+int buzzbuzz = 4;
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+  pinMode(led, OUTPUT);
+  pinMode(buzzbuzz, OUTPUT); 
+}
+
+void loop() {
+  //digitalWrite(8, HIGH);
+  if (mySwitch.available()) {
+
+    digitalWrite(led, HIGH);
+    Serial.print("Received ");
+    Serial.print( mySwitch.getReceivedValue() );
+    Serial.print(" / ");
+    Serial.print( mySwitch.getReceivedBitlength() );
+    Serial.print("bit ");
+    Serial.print("Protocol: ");
+    Serial.println( mySwitch.getReceivedProtocol() );
+
+    mySwitch.resetAvailable();
+    for(int x =0; x<255; x++){
+      analogWrite(buzzbuzz, x);
+      delay(5);
+    }
+    digitalWrite(buzzbuzz, LOW);
+  }
+}
